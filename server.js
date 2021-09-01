6028bea14ad47e32058e9739047279314c72c63a
Aman Pardeshi <85147728+snow-leopard01@users.noreply.github.com>
2021-09-01 12:47:02 +0530

    updating database details

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "Asura@06",
-    database: "smart-brain",
+    host: "ENTER URL OF DATABASE",
+    user: "ENTER POSTGRES USERNAME",
+    password: "ENTER DATABASE PASSWORD",
+    database: "ENTER DATABASE NAME",
   },
 });
 
